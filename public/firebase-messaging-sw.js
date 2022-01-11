cbd9cab681a7506b9088c127b8f9cb57beb813d3
Hendri Agustino <hendri.a@weefer.co.id>
2022-01-11 10:43:42 +0700

    [HA] - Removed: firebase configuration keys

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,12 +3,12 @@ importScripts('https://www.gstatic.com/firebasejs/7.19.1/firebase-messaging.js')
 
 // change the value(s) for the Config variable below according to Firebase Project
 const firebaseConfig = {
-  apiKey: 'AIzaSyBAcKwqsVfkydJ49_3trSocpnR13Vt3CeU',
-  authDomain: 'membership-428a3.firebaseapp.com',
-  projectId: 'membership-428a3',
-  storageBucket: 'membership-428a3.appspot.com',
-  messagingSenderId: '298738312519',
-  appId: '1:298738312519:web:9967b29b859aac58e0d862',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
 
 firebase.initializeApp(firebaseConfig);
